8c99c1ad468d2ac9fe183532eb18540a3d44683a
Mekazara <mekuchazara@gmail.com>
2020-12-22 09:45:05 +0600

    new pathaes for templates

--- gastrotourism/mainapp/urls.py
+++ gastrotourism/mainapp/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import tourList
+from .views import *
 
 urlpatterns = [
-    path('', tourList, name='tours')
+    path('', tourList, name='tours'),
+    path('tourorder/<int:tour_id>/', tourOrder, name='tourorder'),
+    path('tourorders/', orderList, name='order-list'),
 ]
+
