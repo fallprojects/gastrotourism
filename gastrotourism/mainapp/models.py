9d900bd4b2bac706765c265ef4bbf9c872507bf9
Mekazara <mekuchazara@gmail.com>
2020-12-23 09:41:02 +0600

    modified class to make order

--- gastrotourism/mainapp/models.py
+++ gastrotourism/mainapp/models.py
@@ -1,22 +1,7 @@
 from django.db import models
 
-
 # Create your models here.
 from accounts.models import Customer
-
-
-class Tour(models.Model):
-    durations = (('3 days', '3 days'),
-                 ('5 dsys', '5 days'),
-                 ('7 days', '7 days'))
-    name = models.CharField(max_length=20)
-    description = models.TextField()
-    image = models.ImageField(blank=True, default='default.png')
-    duration = models.CharField(max_length=20, choices=durations)
-    price = models.IntegerField()
-
-    def __str__(self):
-        return self.name + ' ' + self.duration
 
 
 class MainPage(models.Model):
@@ -27,17 +12,36 @@ class MainPage(models.Model):
     def __str__(self):
         return self.title
 
+
+class Tour(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField(blank=True, default='default.png')
+    price = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class PreOrder(models.Model):
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    durations = ((3, 3),
+                 (5, 5),
+                 (7, 7))
+    duration = models.IntegerField(choices=durations)
+
+
 class Order(models.Model):
     statuses = (
         ('New order', 'New order'),
         ('In process', 'In process'),
         ('Finished', 'Finished')
     )
-    tour = models.ForeignKey(Tour, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(PreOrder, on_delete=models.SET_NULL, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=15, choices=statuses, default='New order')
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.tour.name + ' ' + self.status
-
