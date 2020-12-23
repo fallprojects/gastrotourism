27e14f8a5ab335ea88119dad598b2429d46d6d9a
Mekazara <mekuchazara@gmail.com>
2020-12-23 09:42:27 +0600

    new path for tour detail

--- gastrotourism/mainapp/urls.py
+++ gastrotourism/mainapp/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', tourList, name='tours'),
     path('tourorder/<int:tour_id>/', tourOrder, name='tourorder'),
+    path('tour-detail/<int:tour_id>', tourDetail, name='tour-detail'),
     path('tourorders/', orderList, name='order-list'),
 ]
 
